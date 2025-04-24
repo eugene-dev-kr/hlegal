6cf8ed0df1516308cb7ae9c1fa60dde339a869d3
Eugene Martynenko <feliz22@ukr.net>
2025-04-24 19:29:37 +0300

    Reinitialize project structure. Add images for header. Style header

---
+++ js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    //MOBILE MENU
+    const burgerBtn = document.querySelector('.header__burger');
+    const mobileMenu = document.querySelector('.mobile__nav');
+    const closeMobileManu = document.querySelector('.mobile__nav-btn-close');
+    burgerBtn.addEventListener('click', ()=> {
+        console.log('work');
+        mobileMenu.classList.add('visible');
+    });
+    closeMobileManu.addEventListener('click', ()=> {
+        mobileMenu.classList.remove('visible');
+    })
+    console.log(burgerBtn);
+
+
+    //SWITCHED HEADER LANG
+    const langSwitchBtn = document.querySelectorAll('.header__top-langs-switch');
+    langSwitchBtn.forEach(button => {
+        button.addEventListener('click',()=> {
+            langSwitchBtn.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        })
+    })
+
+    //MOBILE CURRENT PAGE MENU
+    const mobileMenuBtn = document.querySelectorAll('.mobile__nav-link');
+    mobileMenuBtn.forEach(button => {
+        button.addEventListener('click',()=> {
+            mobileMenuBtn.forEach(btn => btn.classList.remove('current'));
+            button.classList.add('current');
+        })
+    })
+
+});
